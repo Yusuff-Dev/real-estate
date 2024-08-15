1a933cf1b2e9b8b6f4605f459879d61ff35fd358
Yusuf <abrorov.muxriddin@gmail.com>
2024-08-16 02:28:56 +0500

    new section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -232,7 +232,7 @@ function Home() {
             </div>
 
             <div className="sm:-order-2 md:-order-9">
-              <img src={why1} alt="why 1 image" className="block w-full h-full object-cover"/>
+              <img src={why1} alt="why 1 image" className="block w-full h-full object-cover" />
             </div>
 
             <div className="w-full border border-stroke py-[35px] md:py-[90px] px-4 md:px-[30px] text-black font-Jost md:-order-8">
@@ -241,7 +241,7 @@ function Home() {
             </div>
 
             <div className="sm:-order-1 md:-order-7">
-              <img src={why2} alt="why 2 image" className="block w-full h-full object-cover"/>
+              <img src={why2} alt="why 2 image" className="block w-full h-full object-cover" />
             </div>
 
             <div className="w-full border border-stroke py-[35px] md:py-[90px] px-4 md:px-[30px] text-black font-Jost md:-order-6">
@@ -250,7 +250,7 @@ function Home() {
             </div>
 
             <div className="md:-order-5">
-              <img src={why3} alt="why 3 image" className="block w-full h-full object-cover"/>
+              <img src={why3} alt="why 3 image" className="block w-full h-full object-cover" />
             </div>
 
             <div className="w-full border border-stroke py-[35px] md:py-[90px] px-4 md:px-[30px] text-black font-Jost sm:order-4 md:-order-4">
@@ -258,8 +258,8 @@ function Home() {
               <p className="content">Мы разрабатываем персонализированные решения, учитывая уникальные потребности каждого клиента</p>
             </div>
 
-            <div className="sm:order-3 md:order-0"> 
-            <img src={why4} alt="why 4 image" className="block w-full h-full object-cover"/>
+            <div className="sm:order-3 md:order-0">
+              <img src={why4} alt="why 4 image" className="block w-full h-full object-cover" />
             </div>
 
             <div className="w-full border border-stroke py-[35px] md:py-[90px] px-4 md:px-[30px] text-black font-Jost sm:order-5 md:order-">
@@ -267,8 +267,93 @@ function Home() {
               <p className="content">Полный спектр услуг, включающий аренду, куплю-продажу, управление и инвестиции в недвижимость.</p>
             </div>
 
-            <div className="sm:order-5"> 
-            <img src={why5} alt="why 5 image" className="block md:hidden w-full h-full object-cover"/>
+            <div className="sm:order-5">
+              <img src={why5} alt="why 5 image" className="block md:hidden w-full h-full object-cover" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-[180px] xsm:py-[200px] md:py-[250px]">
+        <div className="container max-w-[1480px] font-Jost">
+          <h2 className="heading-2 font-Jost text-black mb-[25px] md:mb-10">Рассчитайте ипотеку</h2>
+          
+          <div className="drop-shadow-[0_30px_30px_#0F11120D] bg-white py-5 px-5 sm:flex gap-[30px]">
+
+            <div className="flex flex-col gap-5 font-Jost text-black-60 content grow">
+              <div className="flex flex-col md:flex-row gap-5 w-full">
+                <div className="w-full">
+                  <label className="block label">Стоимость недвижимости (у.е.)</label>
+                  <input
+                    type="text"
+                    // value={propertyValue}
+                    // onChange={(e) => setPropertyValue(e.target.value)}
+                    className="border border-stroke p-2 w-full mt-1 outline-none"
+                    placeholder="Введите стоимость"
+                  />
+                </div>
+
+                <div className="w-full">
+                  <label className="block label">Первоначальный взнос (у.е.)</label>
+                  <input
+                    type="text"
+                    // value={downPayment}
+                    // onChange={(e) => setDownPayment(e.target.value)}
+                    className="border border-stroke p-2 w-full mt-1 outline-none"
+                    placeholder="Введите размер взноса"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-5 -w-full">
+                <div className="w-full">
+                  <label className="block label">Срок в годах</label>
+                  <input
+                    type="text"
+                    // value={termYears}
+                    // onChange={(e) => setTermYears(e.target.value)}
+                    className="border border-stroke p-2 w-full mt-1 outline-none"
+                    placeholder="Введите срок"
+                  />
+                </div>
+
+                <div className="w-full">
+                  <label className="block label">Ставка (%)</label>
+                  <input
+                    type="text"
+                    // value={interestRate}
+                    // onChange={(e) => setInterestRate(e.target.value)}
+                    className="border border-stroke p-2 w-full mt-1 outline-none"
+                    placeholder="Введите размер ставки"
+                  />
+                </div>
+              </div>
+              
+              <div className="mt-[30px]">
+              <BtnBeigie>Рассчитать</BtnBeigie>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-5 font-Jost content text-[#B3B3B3] border-t sm:border-l sm:border-t-0 border-stroke mt-[30px] sm:mt-0 py-[30px] sm:py-0 sm:px-10">
+              <div className="flex flex-col md:flex-row gap-5">
+                <div className="">
+                  Ежемесячный платёж:
+                  <p className="heading-4 text-black">4 410 у.е.</p>
+                </div>
+                <div className="">
+                  Сумма кредита:
+                  <p className="heading-4 text-black">200 000 у.е.</p>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-5">
+                <div className="">
+                  Процентная ставка:
+                  <p className="heading-4 text-black">24%</p>
+                </div>
+                <div className="">
+                  Дата последнего платежа:
+                  <p className="heading-4 text-black">28 июля 2034 г</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
